71c9a7b4767735905c4b67aac1e279d7dbc859aa
alexander.horne <alexander_horne@outlook.com>
2019-10-03 11:18:28 +0200

    THP

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -13,6 +13,19 @@ def greet(first_name)
   return "Bonjour #{first_name}"
 end
 
+def ask_last_name
+  puts "Et quel est ton nom de famille ?"
+  print "> "
+  last_name = gets.chomp
+  return last_name
+end
+
+def greet2(first_name, last_name)
+  return "Et bien le bonjour #{first_name} #{last_name}"
+end
+
 puts say_hello
 my_first_name = ask_first_name
 puts greet(my_first_name)
+my_last_name = ask_last_name
+puts greet2(my_first_name, my_last_name)
